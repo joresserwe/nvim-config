87b6f77f8cf8d267a32ddd218cf55f73736e227f
Cyan <joresserwe12@gmail.com>
2026-07-16 01:19:00 +0900

    Use okuuva fork of auto-save.nvim

--- lua/plugins/modules/editing-support/autosave.lua
+++ lua/plugins/modules/editing-support/autosave.lua
@@ -1,8 +1,8 @@
 return {
-  "Pocco81/auto-save.nvim",
+  "okuuva/auto-save.nvim",
+  version = "^1.0.0",
   event = { "User AstroFile", "InsertEnter" },
   opts = {
-    execution_message = { message = "" },
     condition = function(buf)
       if not vim.api.nvim_buf_is_valid(buf) then return false end
       if vim.bo[buf].buftype == "acwrite" then return false end
