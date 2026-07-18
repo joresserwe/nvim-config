41f5ef0d4814eb32bbf74aed5286d0fb8ee69456
Cyan <joresserwe12@gmail.com>
2026-07-18 15:30:42 +0900

    Add reference and implementation pickers to Trouble

--- lua/plugins/modules/ui/trouble.lua
+++ lua/plugins/modules/ui/trouble.lua
@@ -6,6 +6,8 @@ return {
     { "sq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix (Trouble)" },
     { "sd", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
     { "sD", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+    { "sr", "<cmd>Trouble lsp_references toggle<cr>", desc = "References (Trouble)" },
+    { "si", "<cmd>Trouble lsp_implementations toggle<cr>", desc = "Implementations (Trouble)" },
   },
   opts = {
     focus = true,
